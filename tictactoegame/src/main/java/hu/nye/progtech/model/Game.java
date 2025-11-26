23a75429cbff855740cad7e186c406a2bf439da5
Bence Sarga <sargabence26@gmail.com>
2025-11-26 23:41:04 +0100

    Implement missing model classes

---
+++ tictactoegame/src/main/java/hu/nye/progtech/model/Game.java
@@ -0,0 +1,51 @@
+package hu.nye.progtech.model;
+
+public class Game {
+
+    private final Player player;
+    private final Player computer;
+    private final Board board;
+    private Player currentPlayer;
+    private GameStatus status;
+
+    public Game(Player player, Player computer, Board board) {
+        this.player = player;
+        this.computer = computer;
+        this.board = board;
+        this.currentPlayer = player;
+        this.status = GameStatus.IN_PROGRESS;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Player getComputer() {
+        return computer;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
+
+    public void switchCurrentPlayer() {
+        currentPlayer = currentPlayer == player ? computer : player;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "player=" + player + ", computer=" + computer + ", status=" + status + "}";
+    }
+}
---
+++ tictactoegame/src/main/java/hu/nye/progtech/model/GameStatus.java
@@ -0,0 +1,8 @@
+package hu.nye.progtech.model;
+
+public enum GameStatus {
+    IN_PROGRESS,
+    PLAYER_WON,
+    COMPUTER_WON,
+    DRAW
+}
